849832bcea9d38fc729f58ed2c0e6027e5087944
Steven Makunzva <stevenm@ZCTSTEMAK003.local>
2019-08-14 16:12:40 +0200

    Binary search refactor

--- BinarySearch.java
+++ BinarySearch.java
@@ -11,12 +11,12 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 
-		int numIndex = loopArray(theArray,0,theArray.length, 12);
+		int numIndex = recursiveSearch(theArray,0,theArray.length, 12);
 		System.out.print(numIndex);
 
 	}
 
-	public static int loopArray(int[] myArray, int start, int end, int search) {
+	public static int recursiveSearch(int[] myArray, int start, int end, int search) {
 
 		// Break statement for the recursive function
 		if(start >= end) {
@@ -29,13 +29,35 @@ public class BinarySearch {
 			return middle;
 		} else if(myArray[middle] > search) {
 		//If middle element is greater than search then search the left side
-			return loopArray(myArray, start, middle, search);
+			return recursiveSearch(myArray, start, middle, search);
 
 		} else {
 		//If middle element is less than search then search the right side
-			return loopArray(myArray, middle + 1, end, search);
+			return recursiveSearch(myArray, middle + 1, end, search);
 		}
 
+	}
+
+
+	public static int iterativeSearch(int[] myArray, int value) {
+
+		int start = 0;
+		int end = myArray.length;
+
+		while(start < end) {
+
+			int middle = (start + end) /2;
+
+			if(myArray[middle] == search) {
+				return middle;
+			} else if(myArray[middle] > search) {
+				end = middle;
+			} else if(myArray[middle] < search) {
+				start = middle + 1;
+			}
+		}
+
+		return -1;
 	}
 
 }
